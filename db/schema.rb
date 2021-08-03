1f2e61b38e274a5c98a8eafcab22bad214e69a00
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-03 18:17:30 +0700

    add secure password and password field to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_172131) do
+ActiveRecord::Schema.define(version: 2021_08_03_094039) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_172131) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
