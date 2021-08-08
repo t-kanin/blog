28e5972a2388d1f24907c33f60dcca6bf2390c5f
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-08-08 16:29:15 +0700

    complete index and show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
 
     def show 
+        @articles = @category.articles.paginate(page: params[:page], per_page: 4)
     end 
 
     def new 
